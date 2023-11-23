6c223d7dbb4aad48ce035283f2536e673f942fc6
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-11-23 00:59:50 -0500

    same issue

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import dg from "../../src/images/dinner_generator.png"
 
 const Home = () => {
+    const projects = [
+        {title: 'Project 1', description: 'This is the first project', imgsrc: dg},
+        {title: 'Project 2', description: 'This is the second project'},
+        {title: 'Project 3', description: 'This is the third project'},
+        {title: 'Project 4', description: 'This is the fourth project'},
+    ]
     return (
-        <h1>Home</h1>
+        <h1>YLC Portfolio</h1>
     );
 }
 
